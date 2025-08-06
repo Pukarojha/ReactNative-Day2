1f68f47c631553ce0d30d342e0d235e136dfc216
Ojha <ojhap@student.douglascollege.ca>
2025-08-06 15:58:26 -0700

    to be continued..

--- App.js
+++ App.js
@@ -1,28 +1,34 @@
-import {StyleSheet, View, Text, TextInput, Button} from "react-native";
+import { StyleSheet, View, Text, Button, TextInput } from "react-native";
 import {useState} from 'react';
 
 
 export default function App(){
 
     const [enteredText, setEnteredText] = useState('');
-    const[currentGoals, setCurrentGoals] = useState([]);
+    const [currentGoal, setCurrentGoal] = useState([]);
 
-    const addGoalHandler =()=>{
-        setCurrentGoals((currentGoals)=>[...currentGoals, enteredText]);
-        setEnteredText('');
+    function gaolInputHandler(enteredText){
+        setEnteredText(enteredText);
     }
 
+   function addGoalHandler(){
+    setCurrentGoal((currentGoal)=>[
+        ...currentGoal,
+        enteredText
+    ]);
+    setEnteredText('');
+   }
+    
     return(
         <View style= {styles.appContainer}>
-            <View style = {styles.inputContainer}>
-                <TextInput style = {styles.inputField} placeholder="Enter Goals" value = {enteredText} onChangeText={setEnteredText}/>
-                <Button title="Add Goals" onPress={addGoalHandler} />
+            <View style= {styles.inputContainer}>
+                <TextInput style= {styles.textInput} placeholder="Enter Goal"   value ={enteredText} onChangeText={gaolInputHandler}/>
+                <Button title="ADD GOAL" onPress={addGoalHandler}/>
             </View>
-            <View >
-                <Text style= {styles.listGoals} >List of Goals: </Text>
-                {currentGoals.map((goal,index)=><Text key={index}> {goal}</Text>)}
+            <View style= {styles.listGoal}>
+                <Text style = {styles.listOfGoal}>List of Goals:</Text>
+            </View>
 
-            </View>
         </View>
     )
 }
@@ -32,23 +38,32 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         flex: 1,
-    }
-    ,inputContainer:{
+        backgroundColor: 'grey',
+        padding: 3,
+
+    },
+    inputContainer:{
         justifyContent: 'center',
         alignItems: 'center',
-        flexDirection: 'row',
-        paddingBottom: 15
+        flex: 1,
+        flexDirection: 'row'
     },
-    inputField:{
-        height: 30,
-        width: '50%',
-        marginRight: 80,
-        borderRadius: 13,
-        borderColor: 'grey'
-    }
-    , listGoals:{
-        paddingBottom: 19,
-        borderBottomWidth: 1,
-        borderColor: 'grey'
+    textInput: {
+        height: 40,
+        width: 69,
+        marginRight: 38,
+        borderWidth: 1,
+        borderColor: 'black'
+    },
+    listGoal:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderBottomColor: 'black',
+        borderBottomWidth: 3
+    },
+    listOfGoal:{
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
