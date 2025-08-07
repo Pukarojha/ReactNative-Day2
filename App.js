e9a9d840c3bf90c5054eaccc5fd09e21b9cd763f
Ojha <ojhap@student.douglascollege.ca>
2025-08-07 09:19:48 -0700

    day 3 begins here

--- App.js
+++ App.js
@@ -1,34 +1,43 @@
-import { StyleSheet, View, Text, Button, TextInput } from "react-native";
+import {StyleSheet, View, Text, TextInput, Button,ScrollView} from 'react-native';
 import {useState} from 'react';
 
 
 export default function App(){
 
     const [enteredText, setEnteredText] = useState('');
-    const [currentGoal, setCurrentGoal] = useState([]);
+    const [courseGoal, setCourseGoal] = useState([]);
 
-    function gaolInputHandler(enteredText){
+
+    function inputHandler(enteredText){
         setEnteredText(enteredText);
     }
 
-   function addGoalHandler(){
-    setCurrentGoal((currentGoal)=>[
-        ...currentGoal,
-        enteredText
-    ]);
-    setEnteredText('');
-   }
-    
+    function addGoalHandler(courseGoal){
+        setCourseGoal((courseGoal)=>[
+            ...courseGoal, 
+            enteredText
+        ]);
+        setEnteredText('');
+    }
     return(
         <View style= {styles.appContainer}>
-            <View style= {styles.inputContainer}>
-                <TextInput style= {styles.textInput} placeholder="Enter Goal"   value ={enteredText} onChangeText={gaolInputHandler}/>
-                <Button title="ADD GOAL" onPress={addGoalHandler}/>
-            </View>
-            <View style= {styles.listGoal}>
-                <Text style = {styles.listOfGoal}>List of Goals:</Text>
-            </View>
+            <View style = {styles.inputContainer}>
+                <TextInput style = {styles.inputTextField} placeholder='Enter Goals' onChangeText={inputHandler} value = {enteredText}/>
+                <Button title='Add Goal' onPress={addGoalHandler}/>
 
+            </View>
+            <View >
+                <Text style= {styles.listGoal}>List of Goals: </Text>
+                
+             
+            </View>
+            <ScrollView
+                alwaysBounceVertical= {false}
+                showsVerticalScrollIndicator= {true}
+            >
+                {courseGoal.map((goal)=>(<Text style ={styles.listOfGoal} key={goal}>{goal}</Text>))}
+
+            </ScrollView>
         </View>
     )
 }
@@ -37,33 +46,23 @@ const styles = StyleSheet.create({
     appContainer:{
         justifyContent: 'center',
         alignItems: 'center',
-        flex: 1,
-        backgroundColor: 'grey',
-        padding: 3,
-
+        flex: 2,
     },
     inputContainer:{
         justifyContent: 'center',
         alignItems: 'center',
-        flex: 1,
         flexDirection: 'row'
     },
-    textInput: {
+    inputTextField:{
         height: 40,
-        width: 69,
-        marginRight: 38,
-        borderWidth: 1,
-        borderColor: 'black'
+        width: 130,
+        marginRight: 59
     },
     listGoal:{
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderBottomColor: 'black',
-        borderBottomWidth: 3
+        borderBottomWidth: 9,
+        borderBottomColor: 'grey'
     },
     listOfGoal:{
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center'
+        flexDirection: 'column'
     }
 })
